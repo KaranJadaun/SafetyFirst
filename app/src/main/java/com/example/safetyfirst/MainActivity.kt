63a0aa40a2a28f12b279bf9b3b18702aa88c96f9
Karan Jadaun <73349879+KaranJadaun@users.noreply.github.com>
2023-08-15 21:31:33 +0530

    Home Screen UI

--- app/src/main/java/com/example/safetyfirst/MainActivity.kt
+++ app/src/main/java/com/example/safetyfirst/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.example.safetyfirst
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    val permissions = arrayOf(
+        android.Manifest.permission.ACCESS_FINE_LOCATION,
+        android.Manifest.permission.CAMERA,
+        android.Manifest.permission.READ_CONTACTS,
+    )
+    val permissioncode = 22
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        askforPermission()
         val bottomBar = findViewById<BottomNavigationView>(R.id.nav_bar)
         bottomBar.setOnItemReselectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.nav_security -> {
                     inflatefragment(GuardFragment.newInstance())
                 }
+
                 R.id.nav_home -> {
                     inflatefragment(homeFragment.newInstance())
                 }
+
                 R.id.nav_dashboard -> {
                     inflatefragment(MapsFragment())
                 }
+
                 R.id.nav_profile -> {
                     inflatefragment(ProfileFragment.newInstance())
                 }
@@ -29,10 +46,60 @@ class MainActivity : AppCompatActivity() {
             true
         }
         bottomBar.selectedItemId = R.id.nav_home
+        val db = Firebase.firestore
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        val name = currentUser?.displayName.toString()
+        val mail = currentUser?.email.toString()
+        val phonenumber = currentUser?.phoneNumber.toString()
+        val imageurl = currentUser?.photoUrl.toString()
+        val user = hashMapOf(
+            "name" to name,
+            "mail" to mail,
+            "phoneNumber" to phonenumber,
+            "imageUrl" to imageurl
+        )
+db.collection("users").document(mail).set(user).addOnSuccessListener {
+}
+    .addOnFailureListener {
     }
-    private fun inflatefragment(newInstance: Fragment) {
+    db.collection("users")
+        .add(user)
+        .addOnSuccessListener { documentReference ->
+            Log.d("FireStore", "DocumentSnapshot added with ID: ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            Log.w("Firestore89", "Error Adding Document", e)
+        }
+    }
+
+    private fun askforPermission() {
+        ActivityCompat.requestPermissions(this, permissions, permissioncode)
+    }
+     private fun inflatefragment(newInstance: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container, newInstance)
         transaction.commit()
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == permissioncode) {
+            if (allpermissiongranted()) {
+
+            }
+            else {
+
+            }
+        }
+    }
+    private fun allpermissiongranted(): Boolean {
+        for (item in permissions) {
+            if (ContextCompat.checkSelfPermission(this, item) != PackageManager.PERMISSION_GRANTED)
+                return false
+        }
+        return true
     }
 }
--- app/src/main/java/com/example/safetyfirst/homeFragment.kt
+++ app/src/main/java/com/example/safetyfirst/homeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.safetyfirst
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.ContactsContract
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class homeFragment : Fragment() {
 
@@ -56,11 +60,59 @@ class homeFragment : Fragment() {
         )
         val adapter = MemberAdapter(listMembers)
         val recycler = requireView().findViewById<RecyclerView>(R.id.recycler)
-        recycler.layoutManager= LinearLayoutManager(requireContext())
-        recycler.adapter=adapter
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapter
+
+        val inviteAdapter = InviteAdapter(fetchContact())
+        val inviteRecycler = requireView().findViewById<RecyclerView>(R.id.recycler_invite)
+        inviteRecycler.layoutManager =
+            LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        inviteRecycler.adapter = inviteAdapter
+
+        val threedots = requireView().findViewById<ImageView>(R.id.icon_three_dots)
+        threedots.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+        }
+    }
+
+    @SuppressLint("Range")
+    private fun fetchContact(): ArrayList<ContactModel> {
+        val cr=requireActivity().contentResolver
+        val cursor =cr.query(ContactsContract.Contacts.CONTENT_URI,null,null,null,null)
+        val listContacts: ArrayList<ContactModel> = ArrayList()
+
+        if(cursor != null && cursor.count > 0){
+            while( cursor != null && cursor.moveToNext()){
+                val id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID))
+                val name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))
+                val hasPhoneNumber = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))
+                if(hasPhoneNumber> 0){
+                    val pCr = cr.query(
+                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                        null,
+                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                        arrayOf(id),
+                        ""
+                    )
+                    if(pCr != null &&  pCr.count>0){
+                        while(pCr != null&& pCr.moveToNext()){
+                            val phoneNum =pCr.getString(pCr.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                            listContacts.add(ContactModel(name,phoneNum))
+                        }
+                        pCr.close()
+                    }
+                }
+            }
+            if(cursor != null){
+                cursor.close()
+            }
+        }
+        return listContacts
+
     }
     companion object {
         @JvmStatic
         fun newInstance() = homeFragment()
     }
 }
+
